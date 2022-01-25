52e80edade3151f7a4bbf46ee464fa9d76357bf3
IgorPessoa <igorpessoa.dev@gmail.com>
2022-01-25 13:03:01 -0300

    esta na metade do projeto

--- api.js
+++ api.js
@@ -1,29 +1,33 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
-const { send } = require('process');
-const { response } = require('express');
 const cotacao = '/last/USD-BRL,EUR-BRL,BTC-BRL';
-const conversao = '/USD-BRL/1';
-const app = express()
+const conversao = '/USD-BRL/1'; //ou /EUR-BRL/
 
 function user() {
-    var USDBRL = new Array;
-    var EURBRL = new Array;
-    var BTCBRL = new Array;
     axios.get(`https://economia.awesomeapi.com.br${cotacao}`).then(function (response) {
-        USDBRL = response.data.USDBRL
-        EURBRL = response.data.EURBRL
-        BTCBRL = response.data.BTCBRL
+        //console.log(response.data.USDBRL)
+        document.getElementById('cotas').innerHTML = `Dolar: <br id="cotas">${response.data.USDBRL.bid} </br>`;
     }).catch((erro) => {
         console.error(erro);
     });
 };
 function conv() {
-    axios.get(`https://economia.awesomeapi.com.br${conversao}`)
-        .then((response) => {
-            console.log(response.data);
-        }).catch((erro) => {
-            console.error(erro);
-        });
+    let money = document.getElementById('money').value;
+    let moneyConvertido = 0;
+    let cotacao = 0;
+    if (money != "") {
+        // console.log('entro no if')
+        axios.get(`https://economia.awesomeapi.com.br/${conversao}`)
+            .then((response) => {
+                //console.log(response.data);
+                cotacao = response.data[0].bid;
+                moneyConvertido = money / cotacao
+                document.getElementById('cotacao').innerHTML = cotacao;
+                document.getElementById('conversao').innerHTML = `R$ ${moneyConvertido.toFixed(2)}`;
+            }).catch((erro) => {
+                console.error(erro);
+            });
+    } else {
+        //console.log('entro no else')
+        document.getElementById('cotacao').innerHTML = "Adicione um valor em R$";
+    }
 };
+user()
